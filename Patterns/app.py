bfa209db70aeb5cdde69066bdd8772fe09a15721
Abhinav <abhinav2presidency@gmail.com>
2025-11-06 15:18:43 +0530

    Patterns

---
+++ Patterns/app.py
@@ -0,0 +1,60 @@
+#PATTERN 1
+# for i in range(4):
+#     for j in range(4):
+#         print("*",end="")
+#     print()   
+
+# for i in range(1,5): 
+#         print("*"*i,end="")
+#     print()   
+
+#PATTERN 2
+# for i in range(5): 
+#     for j in range(i): 
+#         print("*",end="")
+#     print()   
+
+#PATTERN 3
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print()    
+
+#PATTERN 4
+# for i in range(1,6):
+#     for j in range(i):
+#         print(i,end="")
+#     print()    
+
+# PATTERN 5
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("*",end="")
+#     print()    
+
+#PATTERN 6
+# for i in range(6,1,-1):
+#     for j in range(1,i):
+#         print(j,end="")
+#     print()    
+
+#PATTERN 9 = 7 + 8
+# PATTERN 7
+for i in range(5):
+    for j in range(5-i-1):
+        print(" ",end="")
+    for j in range(2*i+1):
+        print("*",end="")
+    for j in range(5-i-1):
+        print(" " ,end="")
+    print()    
+
+#PATTERN 8
+for i in range(5,0,-1):
+    for j in range(5-i):
+        print(" ",end="")
+    for j in range(2*i-1):
+        print("*",end="")
+    for j in range(5-i):
+        print(" " ,end="")
+    print()    
