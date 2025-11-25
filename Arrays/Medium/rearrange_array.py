1774ebf65bb58ece87690974d4bdaad441e8fe9f
Abhinav <abhinav2presidency@gmail.com>
2025-11-25 09:36:29 +0530

    added rearrange array. I'm very bad at coding. God Save me.

---
+++ Arrays/Medium/rearrange_array.py
@@ -0,0 +1,13 @@
+def rearrange_array(nums):
+    op = [0] * len(nums)
+    pos_ind = 0
+    neg_ind = 1
+
+    for i in range(len(nums)):
+        if nums[i] < 0:
+            op[neg_ind] = nums[i]
+            neg_ind += 2
+        else:
+            op[pos_ind] = nums[i]
+            pos_ind += 2
+    return op            
