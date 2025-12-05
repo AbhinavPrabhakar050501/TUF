578d90081e7f6968eda7cf1f997418b47a7bc885
Abhinav <abhinav2presidency@gmail.com>
2025-12-05 17:59:12 +0530

    Added array sorted rotated check script

---
+++ Arrays/Easy/Check_if_array_sorted_rotated.py
@@ -0,0 +1,14 @@
+from typing import List
+def sort_rot(nums:List[int]) -> bool:
+    # if there are multiple drops it is false: algo
+    n = len(nums)
+    if n <= 2:
+        return True
+
+    drop = 0
+    for i in range(n):
+        if nums[i] > nums[(i+1)%n]: #Modulo lets us compare last → first and close the circle.
+            drop += 1    
+            if drop > 1:
+                return False 
+    return True  
