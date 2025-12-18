a0d1306edd4c20c3862f71c8c6ffe770903822d2
Abhinav <abhinav2presidency@gmail.com>
2025-12-18 18:25:37 +0530

    Week 4 starts(late)

---
+++ Arrays/Hard/Week4/3Sum.py
@@ -0,0 +1,73 @@
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        #STEP 1:
+        #build hashmap with pos :
+        # pos = {}
+        # for i in range(0,len(nums)):
+        #     pos[nums[i]] = i
+        # #{-1:0,0:1}                                                
+        # print(pos)
+        # l,r = 0,1
+        # op = []
+        # while r < len(nums):
+        #     ans = 0 - nums[l] - nums[r]
+        #     if ans in pos and pos[ans] != l and pos[ans] != r:
+        #         op.append([nums[l],nums[r],ans])
+        #     l=l+1
+        #     r=r+1    
+
+        # return op
+
+        #better approach using hashset
+        # ansset = set()
+        # for i in range(0,len(nums)):
+        #     hashset = set()
+        #     for j in range(i+1,len(nums)):
+        #         third = -(nums[i] + nums[j])
+
+        #         if third in hashset:
+        #             #we found a triplet
+        #             triplet = tuple(sorted([nums[i],nums[j],third]))
+        #             ansset.add(triplet)
+
+        #         hashset.add(nums[j])    
+
+        # return [list(triplet) for triplet in ansset]      
+
+        #optimal approach using two pointers
+
+        n = len(nums)
+        nums.sort()
+        op = []
+
+        for i in range(n):
+            if i>0 and nums[i] == nums[i - 1]:
+                continue
+
+            l,r = i+1,n-1 
+            while l<r:
+                total = nums[i] + nums[l] + nums[r]
+                if total == 0:
+                    op.append([nums[i],nums[r],nums[l]])
+                    l += 1
+                    r -= 1
+
+                    while l<r and nums[l] == nums[l-1]:
+                        l+=1
+                    while l<r and nums[r] == nums[r+1]:
+                        r-=1
+
+                elif total < 0:
+                    l+=1
+                else:
+                    r-=1
+
+        return op        
+
+
+
+
+
+
