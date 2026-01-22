87b6a24d579d81f5415332dd39685de085ca2edc
Abhinav <abhinav2presidency@gmail.com>
2026-01-22 17:04:01 +0530

    Week 8 continues

---
+++ Binary_Search/Week8/find_min_rotated_sorted_ii.py
@@ -0,0 +1,30 @@
+from typing import List
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        l,r = 0,len(nums)-1
+        op = float('inf')
+        while l <= r:#single elements so '='
+            mid = (l+r)//2
+            
+            #break out early (not needed step)
+            if nums[l] < nums[r]:
+                op = min(op,nums[l])
+                break
+            
+            #duplicates-> this is part ii
+            if nums[l] == nums[mid] == nums[r]:
+                op = min(nums[l],op)
+                l += 1
+                r -= 1
+                continue   
+
+            #find the sorted part
+            if nums[l] <= nums[mid]:
+                op = min(op,nums[l])
+                l = mid+1
+            else:
+                op = min(op,nums[mid]) #dont get confused draw the prob..easy
+                r = mid-1
+        return op
